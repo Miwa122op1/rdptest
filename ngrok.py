91624e5596e38b97d1b716bc6705bc68519058da
Miwa122op1 <88389757+Miwa122op1@users.noreply.github.com>
2023-07-05 02:00:53 +0300

    Add files via upload

---
+++ ngrok.py
@@ -0,0 +1,7 @@
+import ngrok
+async def create_tunnel(port):
+    session = await ngrok.NgrokSessionBuilder().authtoken_from_env().connect()
+    tunnel = await session.http_endpoint().listen()
+    print (f"Ingress established at {tunnel.url()}")
+    tunnel.forward_tcp(f"localhost:{port}")
+await create_tunnel(3389)
